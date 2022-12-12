f39c58f7621f19ac2145eb41fe7cf79460cb2db1
hamade-kj <hamadekj15@gmail.com>
2022-12-12 20:41:48 +0100

    ls

--- script.sh
+++ script.sh
@@ -13,3 +13,24 @@ backup_files = "/oracle"
 des = "/home/oracle/backup"
 #create archive filename
 day=$ (date + %A)
+hostname=$(hostname -s)
+archive_file ="$hostname-$day.tgz"
+
+#print start status message.
+
+echo "Backing up database to $dest/$archive_file"
+date
+echo 
+
+#backup this file using tar
+
+tar czf $dest/$archive_file $backup_files
+
+#print end status message 
+echo 
+echo "Backup termine "
+date
+
+#verification des fichiers 
+
+ls -lh $dest
